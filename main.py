f6a236919a76e9c2856304ac4714bafa627c633e
LaryssaTavares16 <149209086+LaryssaTavares16@users.noreply.github.com>
2023-10-28 00:58:05 +0000

    terminei

---
+++ main.py
@@ -0,0 +1,17 @@
+nome=input('Digite seu nome:')
+cpf=int(input('Digite seu CPF:'))
+n1=int(input('Data de nascimento:'))
+n2=input('Escolaridade:')
+n3=input('Nome do responsàvel:')
+n4=int(input('Número de telefone:'))
+n5=int(input('Número do responsàvel:'))
+print(f""" 
+\033[1;35:43mCadastro para o curso: 
+\033[0;31m Nome: {nome}  
+\033[0;32m CPF:{cpf}   
+\033[0;34m Data d'nasc:{n1}
+\033[0;36m Escolaridade:{n2}
+\033[0;33m Responsàvel:{n3}
+\033[0;30m Número de telefone:{n4}
+\033[0;35m Número do responsàvel:{n5}
+      """)
